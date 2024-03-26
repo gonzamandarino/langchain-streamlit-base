4157b5b110c1fa4e1e8bc4079d5061de7d1041e2
gonzamandarino <49444871+gonzamandarino@users.noreply.github.com>
2024-03-26 20:07:24 -0300

    all pero con OpenAI

--- base.py
+++ base.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from langchain_community.llms import Ollama
-llm = Ollama(model="llama2")
+from langchain.llms import OpenAI
+
+openai_api_key = st.sidebar.text_input('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')
 
 st.title('🦜🔗 Quickstart App')
-
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
-    if submitted:
-        st.info(llm(text))
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
