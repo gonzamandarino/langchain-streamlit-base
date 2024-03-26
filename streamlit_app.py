d01cb01ae56b0371177335f32cad27d5272dddf1
Mandarino <gonzalo.n.mandarino@accenture.com>
2024-03-26 20:52:50 -0300

    cambio por ollama

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,12 @@
 import streamlit as st
-from langchain.llms import OpenAI
-
-openai_api_key = st.sidebar.text_input('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')
+from langchain_community.llms import Ollama
+llm = Ollama(model="llama2")
 
 st.title('🦜🔗 Quickstart App')
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+    if submitted:
+        st.info(llm(text))
